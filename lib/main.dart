89ba7661880e8398ca3faf888562c71b35d99b1f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-28 12:00:22 +0800

    此提交將新增範例
    修改項目:
    
    模組:
    
    議題

--- lib/main.dart
+++ lib/main.dart
@@ -4,74 +4,77 @@
 // opens a [SnackBar], while the second action navigates to a new page.
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MaterialApp(
+    home: new HomePage(),
+));
 
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = '訂餐好好吃';
-
+class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatelessWidget(),
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('MyApp Demo'),
+      ) ,
+      body: Row(children: <Widget>[
+        Text(
+          'Demo',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w700,
+            color: Colors.green,
+            fontStyle: FontStyle.italic
+          ),
+        ),
+        Container(
+          color: Colors.greenAccent,
+          width: 10.0,
+          height: 10.0,
+        )
+      ],),
     );
   }
 }
 
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+class HomePage2 extends StatefulWidget{
+  @override
+  HomePage2Sate createState() => HomePage2Sate();
+}
 
+class HomePage2Sate extends State<HomePage2>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Image(image: AssetImage('assets/unnamed.png')),
-        title: const Text('餐點'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.forward),
+        onPressed: (){
+          setState(() {});
+        }
       ),
-      body: ListView(
-        children: const <Widget>[
-          Card(
-            child: ListTile(
-                leading: const Image(image: NetworkImage('https://i.imgur.com/rOX17ph.jpeg')),
-                title: Text('古意得好吃冰淇淋？'),
-                subtitle: Text('新北市新莊區中和街175號'),
-                trailing: Icon(Icons.navigate_next),
-              ),
-          ),
-          Card(
-            child: ListTile(
-              leading: const Image(image: NetworkImage('https://doqvf81n9htmm.cloudfront.net/data/maggieyu_81/180320/0320014.jpg')),
-              title: Text('派森家常小館'),
-              subtitle: Text('台中市西屯區臺灣大道四段108巷24號'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: const Image(image: NetworkImage('https://i.imgur.com/CUG0Aof.jpeg')),
-              title: Text('值得你來漢堡'),
-              subtitle: Text('新北市樹林區備內街46號'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-          ),
-        ],
-      ),
+      appBar: new AppBar(
+        title: Text('MyApp Demo'),
+      ) ,
+      body: Row(children: <Widget>[
+        Container(
+          color: getColor(),
+          width: 100.0,
+          height: 100.0
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 10.0),
+          color: getColor(),
+          width: 100.0,
+          height: 100.0
+        ),
+      ],),
     );
+  }
+  Color getColor() {
+    return Color.fromARGB(
+      255,
+      Random().nextInt(255),
+      Random().nextInt(255),
+      Random().nextInt(255));
   }
 }
