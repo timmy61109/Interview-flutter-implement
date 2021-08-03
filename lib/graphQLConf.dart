27759d896a10c38be64efb1b73d91f02cc461b1d
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-08-03 15:35:02 +0800

    此提交將升級版本到5.0.0
    修改項目:
    
    模組:
    
    議題

--- lib/graphQLConf.dart
+++ lib/graphQLConf.dart
@@ -1,23 +1,27 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import "package:graphql_flutter/graphql_flutter.dart";
-import 'package:graphql/client.dart';
 
 class GraphQLConfiguration {
+
   static HttpLink httpLink = HttpLink(
-    "http://127.0.0.1:8000/graphql",
+    'http://127.0.0.1:8000/graphql',
   );
 
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
+  static ValueNotifier<GraphQLClient> client = ValueNotifier(
     GraphQLClient(
       link: httpLink,
-      cache: GraphQLCache(dataIdFromObject: typenameDataIdFromObject),
+      cache: GraphQLCache(
+        store: InMemoryStore(),
+      ),
     ),
   );
 
   GraphQLClient clientToQuery() {
     return GraphQLClient(
-      cache: GraphQLCache(dataIdFromObject: typenameDataIdFromObject),
       link: httpLink,
+      cache: GraphQLCache(
+        store: InMemoryStore(),
+      ),
     );
   }
 }
