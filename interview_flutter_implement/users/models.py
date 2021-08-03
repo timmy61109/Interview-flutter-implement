711808e616f867664535d30881d57a0a33c44df7
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-08-03 15:38:43 +0800

    此提交將更新登入與註冊條件
    修改項目:
    
    模組:
    
    議題

--- interview_flutter_implement/users/models.py
+++ interview_flutter_implement/users/models.py
@@ -1,15 +1,15 @@
-from django.db import models
-
-# Create your models here.
+"""Create your models here."""
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    """."""
+    """Create the custom user model."""
+
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     username = models.CharField(
-        blank=True, max_length=254,)
+        blank=True, max_length=254, unique=False,)
     email = models.EmailField(
         blank=False, max_length=254, unique=True, verbose_name="email address")
