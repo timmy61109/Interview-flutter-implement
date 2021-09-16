4923353e70a3f72011e91064d76e1bb20afbd127
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-09-16 12:14:35 +0800

    此提交將更新伺服器設定
    修改項目:
    
    模組:
    
    議題

--- interview_flutter_implement/interview_flutter_implement/settings.py
+++ interview_flutter_implement/interview_flutter_implement/settings.py
@@ -177,7 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
